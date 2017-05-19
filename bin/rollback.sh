1fea148b797bb6e5e8776d8b2c4fb309e7d57ded
wangke <wangke59@hotmail.com>
2017-05-19 23:21:54 +0800

    20170519 - 新增一键部署和回滚脚本

---
+++ bin/rollback.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+workspace=/data/aaa
+svn_tmp=$workspace/tmp
+echo "Clean the svn tmp directory..."
+if [ ! -d $svn_tmp ]; then
+  mkdir $svn_tmp
+fi
+rm -rf $svn_tmp/*
+
+echo "Bakup the tale directory..."
+talebk=$workspace/talebk
+if [ ! -d $talebk ]; then
+  mkdir $talebk
+fi
+date=`date +%Y%m%d%H%M%S`
+tar -cvf $workspace/tale-$date.tar $workspace/tale
+mv $workspace/tale-$date.tar $talebk
+
+echo "Checkout tale project from github..."
+svn checkout https://github.com/kekewang/tale/trunk/dist/tale/  $svn_tmp
+cp -r $svn_tmp/* $workspace/tale/
+
+echo "Restarting the nine clock website..."
+cd $workspace/tale/bin
+sh tale.sh stop
+sh tale.sh start
