f146d048bf2cc23436f6bd2210283efc6d3c9c77
kekewang <wangke59@hotmail.com>
2017-10-10 15:37:14 +0800

    20171010 - 修改jdk版本

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 # 在pom.xml目录下执行
 
-#set JAVA_HOME=C:\Program Files\Java\jdk-9
+#set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_144
 #"C:\Program Files (x86)\JetBrains\IntelliJ IDEA 2016.3.1\plugins\maven\lib\maven3\bin\mvn" clean package assembly:single -Pprod -Dmaven.test.skip=true
