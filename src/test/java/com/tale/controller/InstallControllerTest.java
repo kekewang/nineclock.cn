6e35b4655e4828011446751386cf3e7be044ae94
kekewang <wangke59@hotmail.com>
2017-11-13 18:11:58 +0800

    20171113 - 创建Base单元测试

---
+++ src/test/java/com/tale/controller/InstallControllerTest.java
@@ -0,0 +1,18 @@
+package com.tale.controller;
+
+import com.blade.Blade;
+import com.tale.BaseTaleTestCase;
+
+/**
+ * Created by wangke on 2017/11/13.
+ */
+class InstallControllerTest extends BaseTaleTestCase {
+
+    void testIndex() {
+
+    }
+
+    void testDoInstall() {
+
+    }
+}
